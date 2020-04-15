b92f1d9ad9b22a204c24726e9af05622b6572ea8
Marco Antonio Diaz Valdes <44275048+Marco1969theKnight@users.noreply.github.com>
2020-04-15 12:57:40 -0500

    Primer commit

---
+++ Dump20200415.sql
@@ -0,0 +1,132 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: library_db
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `_users`
+--
+
+DROP TABLE IF EXISTS `_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `_users` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `u_fname` varchar(35) NOT NULL,
+  `u_sname1` varchar(35) NOT NULL,
+  `u_sname2` varchar(35) DEFAULT NULL,
+  `u_email` varchar(35) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `_users`
+--
+
+LOCK TABLES `_users` WRITE;
+/*!40000 ALTER TABLE `_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `id_book` int NOT NULL AUTO_INCREMENT,
+  `b_name` varchar(35) NOT NULL,
+  `b_amonunt` int DEFAULT NULL,
+  `b_status` enum('EN STOCK','SIN STOCK') DEFAULT NULL,
+  PRIMARY KEY (`id_book`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books_details`
+--
+
+DROP TABLE IF EXISTS `books_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books_details` (
+  `id_db` int NOT NULL,
+  `bd_description` varchar(35) NOT NULL,
+  `db_pages` int NOT NULL,
+  `db_autor` varchar(125) DEFAULT NULL,
+  PRIMARY KEY (`id_db`),
+  CONSTRAINT `fkbook_db` FOREIGN KEY (`id_db`) REFERENCES `books` (`id_book`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books_details`
+--
+
+LOCK TABLES `books_details` WRITE;
+/*!40000 ALTER TABLE `books_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `books_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loan`
+--
+
+DROP TABLE IF EXISTS `loan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `loan` (
+  `id_user` int NOT NULL,
+  `id_book` int NOT NULL,
+  `l_date` date NOT NULL,
+  `l_status` enum('PENDIENTE','ENTREGADO','VENCIDO') DEFAULT NULL,
+  PRIMARY KEY (`id_user`,`id_book`),
+  KEY `fkbook_loan` (`id_book`),
+  CONSTRAINT `fkbook_loan` FOREIGN KEY (`id_book`) REFERENCES `books` (`id_book`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fkuser_loan` FOREIGN KEY (`id_user`) REFERENCES `_users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loan`
+--
+
+LOCK TABLES `loan` WRITE;
+/*!40000 ALTER TABLE `loan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loan` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-15 12:56:48
